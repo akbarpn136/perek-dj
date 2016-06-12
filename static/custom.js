c4ad83ed4089d6f253cdc1c2ceb27272c5477859
juaradagang.com <syareeefa@gmail.com>
2016-06-12 14:29:04 +0700

    memperbaiki ketinggian untuk menu samping agar 100%

--- static/custom.js
+++ static/custom.js
@@ -9,13 +9,14 @@ $(document).ready(function(){
 
     var lebar_latar_samping_selector = $(".three.wide.column");
     var lebar_latar_samping = lebar_latar_samping_selector.width();
+    var menu_samping_selector = $("div#kolom_samping");
     //var lebar_latar_samping_pad = lebar_latar_samping_selector.css('padding-right').replace("px", "");
     //var lebar_max = lebar_latar_samping;
 
-    $("div#latar-samping").css('width', lebar_latar_samping).css('min-height', $("div#baris").height()+28);
+    $("div#latar-samping").css('width', lebar_latar_samping).css('min-height', menu_samping_selector.height()+28);
     $('.ui.accordion').accordion();
 
-    if($("div#kolom_samping").is(':hidden'))
+    if(menu_samping_selector.is(':hidden'))
     {
         $("body").css('padding-right', 0);
     }
@@ -27,8 +28,8 @@ $(document).ready(function(){
 
     $(window).resize(function(){
         $("div#latar-samping").css('width', lebar_latar_samping_selector.width())
-                                .css('min-height', '100%');
-        if($("div#kolom_samping").is(':hidden'))
+                                .css('min-height', menu_samping_selector.height()+28);
+        if(menu_samping_selector.is(':hidden'))
         {
             $("body").css('padding-right', 0);
         }
