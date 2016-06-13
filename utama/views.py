dd133699160ab64edc3577f3c4bc58e54a30f850
super <juaradagang.com@gmail.com>
2016-06-13 15:24:29 +0700

    membuat fungsi untuk menampilkan halaman kategori

--- utama/views.py
+++ utama/views.py
@@ -4,12 +4,22 @@ from django.contrib import messages
 from django.contrib.auth import logout
 # from django.contrib.auth.decorators import login_required
 
+from .models import Kategori
+
 
 # Create your views here.
 def index(request, ol=None):
     data = {}
     if ol is None or ol == 'offline':
         return render(request, 'utama/halaman_utama.html', data)
+
+
+def lihat_kategori(request):
+    data_kategori = Kategori.objects.all()
+    data = {
+        'kategori': data_kategori
+    }
+    return render(request, 'utama/halaman_kategori.html', data)
 
 
 def keluar(request):
