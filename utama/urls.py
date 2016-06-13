9ee37a67b88181f99d4e4730d8fcb9189dd383af
super <juaradagang.com@gmail.com>
2016-06-13 15:24:07 +0700

    menmabahkan pola url untuk lihat kategori

--- utama/urls.py
+++ utama/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='halaman_utama'),
     url(r'^logout/$', views.keluar, name='halaman_logout'),
+
+    url(r'^kategori/$', views.lihat_kategori, name='halaman_kategori'),
 ]
