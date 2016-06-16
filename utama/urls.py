ca082fceb46a4b77b2c43e821a845907a11e487d
super <juaradagang.com@gmail.com>
2016-06-16 22:52:53 +0700

    menambahkan pola url untuk pencarian kegiatan beserta kelompok kegiatan berdasarkan kategori

--- utama/urls.py
+++ utama/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     url(r'^logout/$', views.keluar, name='halaman_logout'),
 
     url(r'^kategori/$', views.lihat_kategori, name='halaman_kategori'),
+    url(r'^kategori/(?P<slug>[\w-]+)-(?P<pk>[0-9]+)/$', views.kegiatan_berdasarkan_kategori,
+        name='halaman_kegiatan_kategori'),
+    url(r'^cari/(?P<slug>[\w-]+)/$', views.cari_kegiatan, name='halaman_cari_kegiatan'),
+
+    url(r'^json_kegiatan/$', views.bantu_kegiatan, name='json_kegiatan'),
 ]
