610ee60f2ffe6dffaa9ccd7c6c11a259d78210a0
akbarpn136 <akbarpn136@gmail.com>
2016-06-20 14:08:40 +0700

    membuat form untuk modifikasi data kegiatan

--- utama/forms.py
+++ utama/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm, TextInput, SelectMultiple, Textarea
 
 from .models import Kategori, Kegiatan
 
@@ -9,4 +9,18 @@ class FormKategori(ModelForm):
         fields = ['nama']
         widgets = {
             'nama': TextInput(attrs={'placeholder': 'Kategori disini...'})
+        }
+
+
+class FormKegiatan(ModelForm):
+    class Meta:
+        model = Kegiatan
+        exclude = ['slug']
+        widgets = {
+            'kategori_kegiatan': SelectMultiple(
+                attrs={'class': 'ui fluid search dropdown', 'id': 'select_kategori_multi'}),
+            'kapan_dibuat': TextInput(attrs={'class': 'tanggal', 'placeholder': 'Tanggal kegiatan'}),
+            'nama': TextInput(attrs={'placeholder': 'Nama'}),
+            'kode': TextInput(attrs={'placeholder': 'Kode, misal: APPDev, PERK, ...'}),
+            'referensi': Textarea(attrs={'rows': '0'}),
         }
