e1b02ebb098966cc1c00af3c22d1cdfe02fb2e2a
akbarpn136 <akbarpn136@gmail.com>
2016-06-20 11:55:01 +0700

    tambah format model database kedalam panel admin

--- utama/admin.py
+++ utama/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Kategori, Kegiatan
+from .models import Kategori, Kegiatan, Format
 
 
 # Register your models here.
 admin.site.register(Kategori)
 admin.site.register(Kegiatan)
+admin.site.register(Format)
