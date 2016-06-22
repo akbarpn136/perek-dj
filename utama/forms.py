84c2e88e302f76b50190fdfda2b9b232b6336cd0
akbarpn136 <akbarpn136@gmail.com>
2016-06-22 14:14:08 +0700

    membuat form untuk modifikasi data format

--- utama/forms.py
+++ utama/forms.py
@@ -1,6 +1,6 @@
-from django.forms import ModelForm, TextInput, SelectMultiple, Textarea
+from django.forms import ModelForm, TextInput, SelectMultiple, Textarea, Select
 
-from .models import Kategori, Kegiatan
+from .models import Kategori, Kegiatan, Format
 
 
 class FormKategori(ModelForm):
@@ -23,4 +23,16 @@ class FormKegiatan(ModelForm):
             'nama': TextInput(attrs={'placeholder': 'Nama'}),
             'kode': TextInput(attrs={'placeholder': 'Kode, misal: APPDev, PERK, ...'}),
             'referensi': Textarea(attrs={'rows': '0'}),
+        }
+
+
+class FormFormat(ModelForm):
+    class Meta:
+        model = Format
+        exclude = ['format_kegiatan']
+        widgets = {
+            'format_kegiatan': Select(
+                attrs={'class': 'ui fluid search dropdown', 'id': 'select_format'}),
+            'nama': TextInput(attrs={'placeholder': 'Nama'}),
+            'formasi': Textarea(attrs={'rows': '0'}),
         }
