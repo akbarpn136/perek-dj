ee0509fac87349454622bb738b0b9742e4a6a475
juaradagang.com <syareeefa@gmail.com>
2016-06-24 06:59:20 +0700

    mendaftarkan model database personil kedalam panel admin

--- utama/admin.py
+++ utama/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Kategori, Kegiatan, Format
+from .models import Kategori, Kegiatan, Format, Personil
 
 
 # Register your models here.
 admin.site.register(Kategori)
 admin.site.register(Kegiatan)
 admin.site.register(Format)
+admin.site.register(Personil)
