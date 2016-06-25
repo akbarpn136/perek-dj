94965436aab98177dec18590b05ca0cd35ffde3c
juaradagang.com <syareeefa@gmail.com>
2016-06-25 16:23:18 +0700

    membuat model database untuk profil masing-masing personil

--- utiliti/models.py
+++ utiliti/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Profil(models.Model):
+    awal = ''
+    PILIHAN_JENJANG = (
+        (awal, '----'),
+        ('Pertama', 'Perekayasa Pertama'),
+        ('Muda', 'Perekayasa Muda'),
+        ('Madya', 'Perekayasa Madya'),
+        ('Utama', 'Perekayasa Utama'),
+    )
+
+    nip = models.CharField(max_length=50, verbose_name='NIP')
+    pendidikan = models.CharField(max_length=150, verbose_name='Pendidikan')
+    instansi = models.TextField(verbose_name='Nama Lengkap Instansi')
+    instansi_kode = models.CharField(max_length=20, verbose_name='Singkatan Instansi')
+    pangkat = models.TextField(verbose_name='Pangkat/Golongan Ruang/TMT')
+    jabatan = models.CharField(max_length=150, verbose_name='Jabatan')
+    jenjang = models.CharField(max_length=10, verbose_name='Jenjang Perekayasa', choices=PILIHAN_JENJANG, default=awal)
+    user = models.ForeignKey('auth.User', verbose_name='Personil', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Profil'
+
+    def __str__(self):
+        return self.nip
