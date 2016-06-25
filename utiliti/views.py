afab31f57ca68151193d2bec855dc0a14b36d7d3
juaradagang.com <syareeefa@gmail.com>
2016-06-25 19:18:44 +0700

    membuat fungsi untuk lihat profil pribadi

--- utiliti/views.py
+++ utiliti/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
+from .models import Profil
+
 
 # Create your views here.
+@login_required
+def profil(request):
+    try:
+        data_profil = get_object_or_404(Profil, user=request.user.pk)
+    except Http404:
+        data_profil = Profil()
+
+    data = {
+        'profil': data_profil,
+    }
+
+    return render(request, 'utiliti/halaman_profil.html', data)
+
+
+@login_required
+def modifikasi_profil(request):
+    try:
+        data_profil = get_object_or_404(Profil, user=request.user.pk)
+    except Http404:
+        data_profil = Profil()
+
+    data = {
+        'profil': data_profil,
+    }
+
+    return render(request, 'utiliti/halaman_profil.html', data)
