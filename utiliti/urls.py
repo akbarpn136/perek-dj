46cf4be44edb5c012a9ca189bead7655cb99a39b
juaradagang.com <syareeefa@gmail.com>
2016-06-26 07:33:38 +0700

    membuat pola url untuk modifikasi data profil

--- utiliti/urls.py
+++ utiliti/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^profil/$', views.profil, name='halaman_profil'),
+    url(r'^profil/ubah$', views.modifikasi_profil, name='halaman_modifikasi_profil'),
 ]
