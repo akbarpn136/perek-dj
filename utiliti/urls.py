815bab82f914ba9c0d806571d89da7a5a813cf52
juaradagang.com <syareeefa@gmail.com>
2016-06-27 13:36:26 +0700

    membuat pola utl untuk ubah sandi

--- utiliti/urls.py
+++ utiliti/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^profil/$', views.profil, name='halaman_profil'),
     url(r'^profil/ubah$', views.modifikasi_profil, name='halaman_modifikasi_profil'),
+    url(r'^profil/ubah/sandi$', views.ganti_password, name='halaman_modifikasi_sandi'),
 ]
