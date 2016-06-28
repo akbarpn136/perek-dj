beb614bcd9836b230ab43b6d383f40b6d247dd5d
juaradagang.com <syareeefa@gmail.com>
2016-06-28 11:39:36 +0700

    membuat fungsi awal untuk lihat tugas

--- tugas/views.py
+++ tugas/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+from .models import *
 
 
 # Create your views here.
-def index(request):
-    pass
+def cek_keanggotaan(user, pk_kegiatan):
+    anggota_kegiatan = User.objects.filter(personil__personil_kegiatan=pk_kegiatan)
+    return user.pk in anggota_kegiatan.values_list('pk', flat=True)
+
+
+def index(request, pk):
+    if cek_keanggotaan(request.user, pk):
+        data = {}
+
+        return render(request, 'tugas/halaman_tugas_anggota.html', data)
+    else:
+        messages.warning(request, 'Maaf, Anda tidak memiliki hak keanggotaan dari kegiatan ini.')
+        return redirect('halaman_utama')
