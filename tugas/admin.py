07dbfcf11ac4733cb86938a9a082b74851c44750
juaradagang.com <syareeefa@gmail.com>
2016-06-29 11:36:50 +0700

    menambahkan model database tugas kedalam panel admin

--- tugas/admin.py
+++ tugas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import LembarInstruksi, LembarKerja
+
 # Register your models here.
+admin.site.register(LembarInstruksi)
+admin.site.register(LembarKerja)
