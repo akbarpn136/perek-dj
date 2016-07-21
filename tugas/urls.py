e049af514acd585cb4b0ea9718854bdac1caa46c
akbarpn136 <akbarpn136@gmail.com>
2016-07-21 16:11:28 +0700

    menambahkan pola url baru untuk melihat rincian masing-masing li

--- tugas/urls.py
+++ tugas/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/anggota/$', views.index, name='halaman_tugas_anggota'),
+    url(r'^(?P<slug>[\w-]+)-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)/rincian/$', views.lihat_li,
+        name='halaman_tugas_anggota_rinci'),
 ]
