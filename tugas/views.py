cd509489dec9b01ec3864bb052100555fa06086d
akbarpn136 <akbarpn136@gmail.com>
2016-07-21 16:11:54 +0700

    membuat fungsi baru untuk lihat rincian li

--- tugas/views.py
+++ tugas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, Http404
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -20,10 +20,35 @@ def index(request, pk):
 
         data = {
             'instruksi': data_li[:9],
-            'kerja': data_lk[:9],
+            'kerja': data_lk,
+            'pk': pk,
         }
 
         return render(request, 'tugas/halaman_tugas_anggota.html', data)
     else:
         messages.warning(request, 'Maaf, Anda tidak memiliki hak keanggotaan dari kegiatan ini.')
         return redirect('halaman_utama')
+
+
+@login_required
+def lihat_li(request, slug, pk, keg):
+    if slug is None:
+        pass
+
+    data_lk = LembarKerja.objects.filter(li=pk)
+    data_li_semua = LembarInstruksi.objects.filter(kegiatan=keg)
+
+    try:
+        data_li = get_object_or_404(LembarInstruksi, pk=pk)
+    except Http404:
+        messages.warning(request, 'Lembar instruksi tidak ditemukan!')
+        return redirect('halaman_tugas_anggota', pk=keg)
+
+    data = {
+        'instruksi': data_li_semua,
+        'kerja': data_lk,
+        'li': data_li,
+        'keg': keg,
+    }
+
+    return render(request, 'tugas/halaman_tugas_anggota_rinci.html', data)
