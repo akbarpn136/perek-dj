fdfa9b6ef5fb01c6b215c5d74fcd7dfae546151b
akbarpn136 <akbarpn136@gmail.com>
2016-07-22 14:57:56 +0700

    membuat pola url untuk membuat objek json

--- tugas/urls.py
+++ tugas/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/anggota/$', views.index, name='halaman_tugas_anggota'),
+    url(r'^(?P<pk>[0-9]+)/anggota/(?P<usr>[\w.]+)$', views.index, name='halaman_tugas_anggota_filtered'),
     url(r'^(?P<slug>[\w-]+)-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)/rincian/$', views.lihat_li,
         name='halaman_tugas_anggota_rinci'),
+
+    url(r'^(?P<usr>[\w.]+)/anggota/$', views.bantu_gravatar, name='json_bantu_gravatar'),
 ]
