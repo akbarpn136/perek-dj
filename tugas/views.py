8b3f95df97d167f78fac243588ab41047c4d67c6
akbarpn136 <akbarpn136@gmail.com>
2016-07-22 11:22:04 +0700

    membuat fungsi untuk membuat pagination untuk semua li

--- tugas/views.py
+++ tugas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404, Http404
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import LembarInstruksi, LembarKerja
 
@@ -18,10 +19,29 @@ def index(request, pk):
         data_li = LembarInstruksi.objects.filter(kegiatan=pk)
         data_lk = LembarKerja.objects.filter(kegiatan=pk)
 
+        paginator = Paginator(data_li, 12, 1)
+        page = request.GET.get('halaman')
+
+        try:
+            li = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            li = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            li = paginator.page(paginator.num_pages)
+
+        maks = len(paginator.page_range)
+
+        start_number = li.number - 3 if li.number >= 4 else 0
+        end_number = li.number + 2 if li.number <= maks else maks
+        page_range = paginator.page_range[start_number:end_number]
+
         data = {
-            'instruksi': data_li[:9],
+            'instruksi': li,
             'kerja': data_lk,
             'pk': pk,
+            'page_range': page_range,
         }
 
         return render(request, 'tugas/halaman_tugas_anggota.html', data)
