f4ccaee01c089c59a4c341028af3aa289401c63d
akbarpn136 <akbarpn136@gmail.com>
2016-07-26 16:38:38 +0700

    menambahkan beberapa pola url untuk lihat rincian tugas li/lk dan lihat halaman cetak laporan li/lk

--- tugas/urls.py
+++ tugas/urls.py
@@ -5,10 +5,14 @@ from . import views
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/anggota/$', views.index, name='halaman_tugas_anggota'),
     url(r'^(?P<pk>[0-9]+)/anggota/(?P<usr>[\w.]+)$', views.index, name='halaman_tugas_anggota_filtered'),
-    url(r'^(?P<slug>[\w-]+)-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)/rincian/$', views.lihat_li,
-        name='halaman_tugas_anggota_rinci'),
-    url(r'^(?P<slug>[\w-]+)/cetak-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)/rincian/$', views.lihat_li_rinci,
-        name='halaman_tugas_anggota_rinci_cetak'),
+    url(r'^(?P<slug>[\w-]+)/li-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)/rincian/$', views.lihat_li,
+        name='halaman_li_anggota_rinci'),
+    url(r'^(?P<slug>[\w-]+)/lk-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)/rincian-(?P<li>[0-9]+)/$', views.lihat_lk,
+        name='halaman_lk_anggota_rinci'),
+    url(r'^(?P<slug>[\w-]+)/cetak-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)-li/rincian/$', views.lihat_li_rinci,
+        name='halaman_li_anggota_rinci_cetak'),
+    url(r'^(?P<slug>[\w-]+)/cetak-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)-lk/rincian/$', views.lihat_lk_rinci,
+        name='halaman_lk_anggota_rinci_cetak'),
 
     url(r'^(?P<usr>[\w.]+)/anggota/$', views.bantu_gravatar, name='json_bantu_gravatar'),
 ]
