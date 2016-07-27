90f9aa4c60baf31e2e73b577ba15ab6cf529b54f
akbarpn136 <akbarpn136@gmail.com>
2016-07-27 16:51:53 +0700

    membuat pola url untuk penambahan tugas baru

--- tugas/urls.py
+++ tugas/urls.py
@@ -9,10 +9,14 @@ urlpatterns = [
         name='halaman_li_anggota_rinci'),
     url(r'^(?P<slug>[\w-]+)/lk-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)/rincian-(?P<li>[0-9]+)/$', views.lihat_lk,
         name='halaman_lk_anggota_rinci'),
+
     url(r'^(?P<slug>[\w-]+)/cetak-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)-li/rincian/$', views.lihat_li_rinci,
         name='halaman_li_anggota_rinci_cetak'),
     url(r'^(?P<slug>[\w-]+)/cetak-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)-lk/rincian/$', views.lihat_lk_rinci,
         name='halaman_lk_anggota_rinci_cetak'),
+
+    url(r'^(?P<slug>[\w-]+)-(?P<keg>[0-9]+)/tambah/$', views.tambah_li,
+        name='halaman_tambah_li_anggota'),
 
     url(r'^(?P<usr>[\w.]+)/anggota/$', views.bantu_gravatar, name='json_bantu_gravatar'),
 ]
