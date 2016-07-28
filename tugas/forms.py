b54e4a85d52bed1555fdf9ef7c779b7bebeba094
akbarpn136 <akbarpn136@gmail.com>
2016-07-28 16:39:45 +0700

    menambahkan form untuk LI

--- tugas/forms.py
+++ tugas/forms.py
@@ -1,20 +1,20 @@
-from django.forms import ModelForm, TextInput, Textarea, Select, NumberInput
+from django.forms import ModelForm, TextInput, Textarea, Select
 # from django import forms
 
 from .models import *
 
 
-class Form(ModelForm):
+class FormLI(ModelForm):
     class Meta:
         model = LembarInstruksi
         exclude = ['nama', 'kegiatan', 'pemberi', 'angka']
         widgets = {
             'penerima': Select(
                 attrs={'class': 'ui fluid search dropdown', 'id': 'select_orang'}),
-            'tanggal': TextInput(attrs={'placeholder': 'Tanggal pelaksanaan'}),
-            'butir': TextInput(attrs={'placeholder': 'Nomor butir'}),
+            'tanggal': TextInput(attrs={'placeholder': 'Tanggal pelaksanaan', 'class': 'tanggal'}),
+            'butir': TextInput(attrs={'placeholder': 'Nomor butir', 'id': 'butir'}),
             'index': TextInput(attrs={'placeholder': 'Nomor urutan'}),
             'nomor': Textarea(attrs={'rows': '0', 'id': 'nomor'}),
             'referensi': Textarea(attrs={'rows': '0', 'id': 'referensi'}),
-            'isi': Textarea(attrs={'rows': '0', 'id': 'isi'}),
+            'isi': Textarea(attrs={'rows': '18', 'id': 'isi'}),
         }
