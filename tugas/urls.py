25a89bd92114542ea1d1dae8e0c7577679fb76c1
juaradagang.com <syareeefa@gmail.com>
2016-08-06 12:21:39 +0700

    membuat pola url untuk lihat kesepakatan li

--- tugas/urls.py
+++ tugas/urls.py
@@ -19,6 +19,8 @@ urlpatterns = [
         name='halaman_tambah_li_anggota'),
     url(r'^(?P<slug>[\w-]+)-(?P<keg>[0-9]+)/(?P<li>[\w]+).ubah.(?P<kode>[\w]+)/$', views.ubah_li,
         name='halaman_ubah_li_anggota'),
+    url(r'^(?P<slug>[\w-]+)-(?P<keg>[0-9]+)/kesepakatan.(?P<li>[0-9]+)/$', views.kesepakatan_li,
+        name='halaman_kesepakatan_li_anggota'),
     url(r'^hapus-(?P<pk>[0-9]+)/li/$', views.hapus_li, name='halaman_hapus_li_anggota'),
 
     url(r'^(?P<usr>[\w.]+)/anggota/$', views.bantu_gravatar, name='json_bantu_gravatar'),
