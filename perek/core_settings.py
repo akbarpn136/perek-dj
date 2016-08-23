33870ff45d66cc9a9d27ec18fefa6dabd85b76f8
sniper <juaradagang.com@gmail.com>
2016-08-23 13:22:56 +0700

    menambahkan sub aplikasi ke aplikasi utama

--- perek/core_settings.py
+++ perek/core_settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'tugas.apps.TugasConfig',
     'butir.apps.ButirConfig',
     'logbook.apps.LogbookConfig',
+    'keputusan.apps.KeputusanConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
