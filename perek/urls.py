29648e6b1e057d2299e70d992883d8bb48302eb0
sniper <juaradagang.com@gmail.com>
2016-08-26 15:24:13 +0700

    memperbaiki pola url lembar keputusan dalam aplikasi utama

--- perek/urls.py
+++ perek/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     url(r'^utiliti/', include('utiliti.urls')),
     url(r'^tugas/', include('tugas.urls')),
     url(r'^logbook/', include('logbook.urls')),
-    url(r'^keputusan/', include('keputusan.urls')),
+    url(r'^tugas/keputusan/', include('keputusan.urls')),
 ]
