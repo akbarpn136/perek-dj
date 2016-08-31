a1e6b076e4af3db303313f7d50db8d818c388acc
akbarpn136 <akbarpn136@gmail.com>
2016-08-31 16:40:00 +0700

    membuat pola url untuk lihat /cetak lembar keputusan

--- keputusan/urls.py
+++ keputusan/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<slug>[\w-]+)-(?P<pk>[0-9]+)/rincian/$', views.LihatKeputusan.as_view(), name='halaman_keputusan'),
+    url(r'^(?P<slug>[\w-]+)-(?P<pk_kegiatan>[0-9]+)-(?P<pk>[0-9]+)/rincian$', views.RincianKeputusan.as_view(),
+        name='halaman_rincian_keputusan'),
 
     # url(r'^(?P<slug>[\w-]+)/cetak-(?P<pk>[0-9]+)-(?P<keg>[0-9]+)-lb/rincian@(?P<kode>[\w]+)/$', views.lihat_lb_rinci,name='halaman_lb_anggota_rinci_cetak'),
 
