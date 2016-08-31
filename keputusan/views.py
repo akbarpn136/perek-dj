445021f0ad38404b25cdbb98206bb3cc6bbb07d0
akbarpn136 <akbarpn136@gmail.com>
2016-09-01 06:43:06 +0700

    membuat metoda untuk tambah keputusan

--- keputusan/views.py
+++ keputusan/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, get_object_or_404, Http404, redirect
+from django.shortcuts import get_object_or_404, Http404, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.views import generic
+from django.views.generic import edit
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 from .models import LembarKeputusan
 from utama.models import Kegiatan
+from .forms import FormKeputusan
 
 
 # Create your views here.
@@ -14,6 +18,7 @@ def cek_keanggotaan(user, pk_kegiatan):
     return user.pk in anggota_kegiatan.values_list('pk', flat=True)
 
 
+@method_decorator(login_required, name='dispatch')
 class LihatKeputusan(generic.ListView):
     model = LembarKeputusan
     template_name = 'keputusan/halaman_keputusan.html'
@@ -37,13 +42,14 @@ class LihatKeputusan(generic.ListView):
         return context
 
 
+@method_decorator(login_required, name='dispatch')
 class RincianKeputusan(generic.DetailView):
     model = LembarKeputusan
     template_name = 'keputusan/halaman_cetak_keputusan_lembaran.html'
     context_object_name = 'keputusan'
 
     def dispatch(self, request, *args, **kwargs):
-        if cek_keanggotaan(request.user, self.kwargs['pk']):
+        if cek_keanggotaan(request.user, self.kwargs['pk_kegiatan']):
             return super(RincianKeputusan, self).dispatch(request, *args, **kwargs)
         else:
             messages.warning(request, 'Maaf, Anda tidak memiliki hak keanggotaan dari kegiatan ini.')
@@ -53,16 +59,44 @@ class RincianKeputusan(generic.DetailView):
         try:
             data_kegiatan = get_object_or_404(Kegiatan, pk=self.kwargs['pk_kegiatan'])
         except Http404:
-            messages.warning('Halaman yang Anda cari tidak ditemukan')
+            messages.warning(self.request, 'Halaman yang Anda cari tidak ditemukan')
             return redirect('halaman_utama')
 
         try:
             data_keputusan = get_object_or_404(LembarKeputusan, pk=self.kwargs['pk_kegiatan'])
         except Http404:
-            messages.warning('Halaman yang Anda cari tidak ditemukan')
+            messages.warning(self.request, 'Halaman yang Anda cari tidak ditemukan')
             return redirect('halaman_utama')
 
         context = super(RincianKeputusan, self).get_context_data(**kwargs)
         context['peran'] = [data_keputusan.pemberi, data_kegiatan.pk]
+
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class TambahKeputusan(edit.CreateView):
+    model = LembarKeputusan
+    template_name = 'keputusan/halaman_keputusan_anggota_modifikasi.html'
+    form_class = FormKeputusan
+
+    def dispatch(self, request, *args, **kwargs):
+        if cek_keanggotaan(request.user, self.kwargs['pk']):
+            return super(TambahKeputusan, self).dispatch(request, *args, **kwargs)
+        else:
+            messages.warning(request, 'Maaf, Anda tidak memiliki hak keanggotaan dari kegiatan ini.')
+            return redirect('halaman_utama')
+
+    def get_context_data(self, **kwargs):
+        try:
+            data_kegiatan = get_object_or_404(Kegiatan, pk=self.kwargs['pk'])
+        except Http404:
+            messages.warning(self.request, 'Halaman yang Anda cari tidak ditemukan')
+            return redirect('halaman_utama')
+
+        context = super(TambahKeputusan, self).get_context_data(**kwargs)
+        context['peran'] = [self.request.user, data_kegiatan.pk]
+        context['pk'] = data_kegiatan.pk
+        context['kegiatan'] = data_kegiatan
 
         return context
