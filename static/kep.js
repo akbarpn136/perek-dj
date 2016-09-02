c0c9a554066b030a07b46adaeafe47ffd91e3ac4
sniper <juaradagang.com@gmail.com>
2016-09-02 11:13:32 +0700

    menambahkan fungsi jquery untuk fungsionalitas lembar keputusan

--- static/kep.js
+++ static/kep.js
@@ -3,6 +3,7 @@
  */
 
 $(document).ready(function(){
+    $("div#label_info").hide();
     $("input.tanggal").Zebra_DatePicker({
         show_icon: false,
         onSelect: function(a, b){
@@ -21,6 +22,8 @@ $(document).ready(function(){
     tinymce.init({
         selector: 'textarea#isi',
         theme: 'modern',
+        resize: false,
+        height : "480",
         plugins: [
             'advlist autolink lists link image charmap print preview hr anchor pagebreak',
             'searchreplace wordcount visualblocks visualchars code fullscreen',
@@ -44,5 +47,61 @@ $(document).ready(function(){
     $("select#select_orang").dropdown({
         placeholder: true,
         fullTextSearch: 'exact'
+    });
+
+    $("a#tbl_butir").click(function(e){
+        $("div#modal_butir").modal({
+            closable  : false,
+            onShow : function(){
+                var selector = $("div#latar-samping");
+                selector.css('min-height', selector.height()+28);
+            }
+        }).modal('show');
+
+        e.preventDefault();
+    });
+
+    $("select#pilih_butir").dropdown({
+        placeholder: true,
+        fullTextSearch: 'exact',
+        onChange: function(butir){
+            var info = $("div#label_info");
+            var keg = $("span#kegiatan").attr('data-value');
+
+            $("input#butir").val(butir);
+
+            $.get('/tugas/'+butir+'/butir/'+keg+'/', function(nilai){
+                $("input#angka_hid").val(nilai['angka']);
+                if(nilai['jenjang'] == '')
+                {
+                    info.text('Hasil: Data profil jenjang masih kosong, Harap diisi terlebih dahulu.');
+                }
+
+                else
+                {
+                    info.text('Hasil: '+nilai['hasil']+', Angka: '+nilai['angka']+' ('+nilai['persentase']+'% dari '+nilai['angka_asli']+')');
+                }
+
+                info.show();
+            });
+        }
+    });
+
+    $("a#tbl_nmr").click(function(e){
+        var nomor = $('textarea#nomor');
+        var baru = $("span#format").attr('data-value');
+
+        baru = baru.replace('%nomor_urut%', '00')
+            .replace('%kode_peran_pemberi%', $("span#kode_peran_pemberi").attr('data-value'))
+            .replace('%kode_wbs_wp%', $("input#id_tingkatan").val())
+            .replace('%kode_peran_penerima%', $("span#kode_peran_penerima").attr('data-value'))
+            .replace('%kode_kegiatan%', $("span#kode_kegiatan").attr('data-value'))
+            .replace('%bulan%', $("span#bulan").attr('data-value'))
+            .replace('%tahun%', $("span#tahun").attr('data-value'));
+
+        nomor.text();
+
+        nomor.text(baru);
+        e.preventDefault();
     });
 });
