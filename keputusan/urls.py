22f38766ad3d774e5d79c555a35386f248af9763
akbarpn136 <akbarpn136@gmail.com>
2016-09-03 14:07:36 +0700

    menambahkan pola url untuk hapus keputusan

--- keputusan/urls.py
+++ keputusan/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
         name='halaman_tambah_keputusan'),
     url(r'^(?P<slug>[\w-]+)-(?P<pk_kegiatan>[0-9]+)-(?P<pk>[0-9]+)/ubah', views.UbahKeputusan.as_view(),
         name='halaman_ubah_keputusan'),
+    url(r'^(?P<slug>[\w-]+)-(?P<pk_kegiatan>[0-9]+)-(?P<pk>[0-9]+)/hapus', views.HapusKeputusan.as_view(),
+        name='halaman_hapus_keputusan'),
 
     url(r'^(?P<cond>[\w.()]+)/butir/(?P<keg>[0-9]+)/$', views.bantu_butir_perekayasa,
         name='json_bantu_butir_perekayasa'),
