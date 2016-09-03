39b8f88f9460929e424d2b4d60c4bfee080310e4
akbarpn136 <akbarpn136@gmail.com>
2016-09-03 11:06:37 +0700

    menambahkan metoda untuk ubah keputusan

--- keputusan/views.py
+++ keputusan/views.py
@@ -94,6 +94,9 @@ class TambahKeputusan(edit.CreateView):
             messages.warning(request, 'Maaf, Anda tidak memiliki hak keanggotaan dari kegiatan ini.')
             return redirect('halaman_utama')
 
+    def get_initial(self):
+        return {'pemberi': self.request.user}
+
     def get_context_data(self, **kwargs):
         try:
             data_kegiatan = get_object_or_404(Kegiatan, pk=self.kwargs['pk'])
@@ -145,3 +148,80 @@ class TambahKeputusan(edit.CreateView):
 
     def get_success_url(self):
         return reverse_lazy('halaman_keputusan',  kwargs={'slug': self.kwargs['slug'], 'pk': self.kwargs['pk']})
+
+
+@method_decorator(login_required, name='dispatch')
+class UbahKeputusan(edit.UpdateView):
+    model = LembarKeputusan
+    template_name = 'keputusan/halaman_keputusan_anggota_modifikasi.html'
+    form_class = FormKeputusan
+
+    def dispatch(self, request, *args, **kwargs):
+        if cek_keanggotaan(request.user, self.kwargs['pk_kegiatan']):
+            try:
+                data_peran = get_object_or_404(Personil, orang=request.user, personil_kegiatan=self.kwargs['pk'],
+                                               peran_utama=True)
+                peran = data_peran.peran
+            except Http404:
+                peran = ''
+
+            if peran not in ['TS', 'ES', 'ACE']:
+                return super(UbahKeputusan, self).dispatch(request, *args, **kwargs)
+            else:
+                messages.warning(request, 'Maaf, Anda tidak mendapatkan izin untuk tambah surat keputusan')
+                return redirect('halaman_utama')
+        else:
+            messages.warning(request, 'Maaf, Anda tidak memiliki hak keanggotaan dari kegiatan ini.')
+            return redirect('halaman_utama')
+
+    def get_context_data(self, **kwargs):
+        try:
+            data_kegiatan = get_object_or_404(Kegiatan, pk=self.kwargs['pk_kegiatan'])
+        except Http404:
+            messages.warning(self.request, 'Halaman yang Anda cari tidak ditemukan')
+            return redirect('halaman_utama')
+
+        try:
+            data_format = get_object_or_404(Format, format_kegiatan=self.kwargs['pk_kegiatan'], kode='DS')
+        except Http404:
+            messages.warning(self.request, 'Halaman yang Anda cari tidak ditemukan')
+            return redirect('halaman_utama')
+
+        data_butir = bantu_peran(self.request.user, 'Lembar Keputusan')
+
+        context = super(UbahKeputusan, self).get_context_data(**kwargs)
+        context['peran'] = [self.request.user, data_kegiatan.pk]
+        context['pk'] = data_kegiatan.pk
+        context['kegiatan'] = data_kegiatan
+        context['butir'] = data_butir
+        context['format'] = data_format
+
+        return context
+
+    def get_form(self, form_class=None):
+        form = super(UbahKeputusan, self).get_form(form_class)
+        form.fields['pemberi'].choices = [('', '------')] + [(user.pk, user.get_full_name()) for user in
+                                                             User.objects.filter(
+                                                                 personil__personil_kegiatan=self.kwargs['pk_kegiatan']
+                                                             ).distinct()]
+
+        return form
+
+    def form_valid(self, form):
+        butir = self.request.POST.get('butir')
+        angka = self.request.POST.get('angka_hid')
+        cek_angka = round(bantu_butir_perekayasa(self.request, butir, self.kwargs['pk_kegiatan'], 'na'), 3)
+
+        if float(angka) == cek_angka:
+            form.instance.angka = angka
+            form.instance.kegiatan = Kegiatan.objects.get(pk=self.kwargs['pk_kegiatan'])
+            form.instance.pemberi = self.request.user
+
+            messages.success(self.request, 'Lembar keputusan berhasil ditambahkan')
+            return super(UbahKeputusan, self).form_valid(form)
+        else:
+            messages.warning(self.request, 'Tidak diperbolehkan untuk mengganti angka kredit!')
+            return redirect('halaman_keputusan', slug=self.kwargs['slug'], keg=self.kwargs['pk_kegiatan'])
+
+    def get_success_url(self):
+        return reverse_lazy('halaman_keputusan',  kwargs={'slug': self.kwargs['slug'], 'pk': self.kwargs['pk_kegiatan']})
